2e59ae0ad2f5c4d223fead007c9685fa901b20ea
its-alikhoker <iamaliraza777@gmail.com>
2025-04-28 15:46:11 +0500

    Refactor: pllease as a extra but need custom add extra field in user to add a custom field name "gl user" and "gl password" the password hidden but need know how from a api decript this to use in other system. Tanks.

---
+++ sam/patches/user_field.py
@@ -0,0 +1,15 @@
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_field
+
+def execute():
+
+    df = dict(fieldname="section_break434", fieldtype="Section Break", label="GL User", insert_after="user_image")
+    df1 = dict(fieldname="gl_user", fieldtype="Data", label="GL User", insert_after="section_break434")
+    df3 = dict(fieldname="column_break2333", fieldtype="Column Break", insert_after="gl_user")
+    df4 = dict(fieldname="gl_password", fieldtype="Data", label="GL Password",  insert_after="column_break2333")
+    create_custom_field("User", df)
+    create_custom_field("User", df1)
+    create_custom_field("User", df3)
+    create_custom_field("User", df4)
+    
+    frappe.db.commit()
